67a8e68669540196dbb6d8c484b174208681cc92
samanthamarberger <slatham04@gmail.com>
2023-07-24 11:10:13 -0600

    Done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,7 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
 
   def index
     toys = Toy.all
@@ -7,17 +9,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toys.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = Toy.find(params[:id])
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = Toy.find(params[:id])
     toy.destroy
     head :no_content
   end
@@ -26,6 +29,14 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_not_found_response
+    render json: { error: "Toy not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,6 @@
 class Toy < ApplicationRecord
+
+    validates :name, presence: true
+    validates :image, presence: true
+    
 end
